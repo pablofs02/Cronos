8ca2d36072c76b6075e0bff60774cf86c7f32141
TetrisRubik <fpablo960@gmail.com>
2022-07-11 18:01:34 +0200

    Nuevos Atributos de Entrada Para mejorar el objeto de evento actual se ha cambiado la info por el nombre y se ha añadido el comentario la fecha de inicio y fin.

--- funciones/creación.js
+++ funciones/creación.js
@@ -1,10 +1,13 @@
 const entrada = document.getElementById("entrada");
 
-entrada.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const datos = {
-		texto: e.target.info.value
+entrada.addEventListener("submit", (evento) => {
+	evento.preventDefault();
+	const data = {
+		nombre: evento.target.nombre.value,
+		comentario: evento.target.comentario.value,
+		inicio: evento.target.inicio.value,
+		fin: evento.target.fin.value
 	};
-	console.log(datos);
+	console.log(data);
 	entrada.reset();
 });
