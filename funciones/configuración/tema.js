ea109e62adfc722f419d06955a689b4d9fc52075
TetrisRubik <fpablo960@gmail.com>
2022-07-25 17:14:42 +0200

    Tema Constante Ahora el tema seleccionado se mantiene entre pestañas y movimientos de página.

--- funciones/configuración/tema.js
+++ funciones/configuración/tema.js
@@ -4,17 +4,35 @@ const nodo_tema = document.getElementById("tema");
 const tema_oscuro = "estilos/oscuro.css";
 const tema_claro = "estilos/claro.css";
 
+if (!localStorage.getItem("tema"))
+	localStorage.setItem("tema", tema_actual());
+else
+	poner_tema(localStorage.getItem("tema"));
+
 botón_tema.addEventListener("click", () => {
 	cambiar_tema();
 });
 
 function cambiar_tema() {
-	if (nodo_tema.getAttribute("href") === tema_oscuro)
+	if (tema_actual() === tema_oscuro)
 		poner_tema_claro();
-	else if (nodo_tema.getAttribute("href") === tema_claro)
+	else if (tema_actual() === tema_claro)
 		poner_tema_oscuro();
 	else
 		console.error("Estilo de tema desconocido.");
+}
+
+function tema_actual() {
+	return nodo_tema.getAttribute("href");
+}
+
+function poner_tema(tema) {
+	if (tema === tema_oscuro)
+		poner_tema_oscuro();
+	else if (tema === tema_claro)
+		poner_tema_claro();
+	else
+		console.error("No se puede poner tema desconocido.");
 }
 
 function poner_tema_oscuro() {
@@ -24,6 +42,8 @@ function poner_tema_oscuro() {
 	nodo_tema.setAttribute("href", tema_oscuro);
 	botón_tema.setAttribute("src", icono_tema_claro);
 	botón_tema.setAttribute("alt", alternativa_tema_claro);
+
+	localStorage.setItem("tema", tema_oscuro);
 }
 
 function poner_tema_claro() {
@@ -33,4 +53,6 @@ function poner_tema_claro() {
 	nodo_tema.setAttribute("href", tema_claro);
 	botón_tema.setAttribute("src", icono_tema_oscuro);
 	botón_tema.setAttribute("alt", alternativa_tema_oscuro);
+
+	localStorage.setItem("tema", tema_claro);
 }
