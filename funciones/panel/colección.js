5163294eb1168e9cff0052e90e7af06e00213049
TetrisRubik <fpablo960@gmail.com>
2022-07-31 14:35:44 +0200

    Descargar y Borrar Tempos En la colección está la opción de borrar o descargar el JSON de un tempo específico.

--- funciones/panel/colección.js
+++ funciones/panel/colección.js
@@ -1,4 +1,4 @@
-import { listar_tempos, guardar_tempo } from "../utilidad/almacenamiento.js";
+import { listar_tempos, guardar_tempo, borrar_tempo } from "../utilidad/almacenamiento.js";
 
 const botón_cargar_tempo = document.getElementById("cargar_tempo");
 const cargar_tempo_real = document.getElementById("cargar_real");
@@ -18,7 +18,6 @@ const tabla = "Tempos";
 listar_tempos(base, tabla).then((lista) => {
 	colocar_lista(lista);
 });
-
 
 function almacenar_archivos(archivos) {
 	for (let i = 0; i < archivos.length; i++) {
@@ -42,6 +41,7 @@ function colocar_lista(lista) {
 			imágen.setAttribute("alt", "Imágen del tempo " + tempo.nombre);
 			nodo.appendChild(imágen);
 			const texto = document.createElement("div");
+			texto.setAttribute("class", "texto_tempo");
 			const título = document.createElement("h3");
 			título.textContent = tempo.nombre;
 			texto.appendChild(título);
@@ -50,13 +50,49 @@ function colocar_lista(lista) {
 			texto.appendChild(comentario);
 			nodo.appendChild(texto);
 
-			nodo.addEventListener("click", () => {
-				localStorage.setItem("tempo", tempo.nombre);
-				location.replace("visualizar.html");
+			imágen.addEventListener("click", () => {
+				ver_tempo(tempo);
+			});
+			texto.addEventListener("click", () => {
+				ver_tempo(tempo);
+			});
+
+			const opciones = document.createElement("div");
+			opciones.setAttribute("class", "opciones_tempo");
+			const borrar = document.createElement("div");
+			borrar.setAttribute("class", "eliminar");
+			borrar.innerHTML = "<i class=\"fa-solid fa-trash-can\"></i>"
+			opciones.appendChild(borrar);
+			const descargar = document.createElement("div");
+			descargar.setAttribute("class", "descargar");
+			descargar.innerHTML = "<i class=\"fa-solid fa-download\"></i>";
+			opciones.appendChild(descargar);
+			nodo.appendChild(opciones);
+
+			borrar.addEventListener("click", () => {
+				if (confirm("¿Estás seguro de que deseas borrarlo?")) {
+					borrar_tempo(base, tabla, tempo);
+					location.reload();
+				}
+			});
+			descargar.addEventListener("click", () => {
+				descargar_objeto(JSON.stringify(tempo), tempo.nombre);
 			});
 
 			fragmento.appendChild(nodo);
 		}
 		colección.appendChild(fragmento);
 	}
+}
+
+function ver_tempo(tempo) {
+	localStorage.setItem("tempo", tempo.nombre);
+	location.replace("visualizar.html");
+}
+
+function descargar_objeto(objeto, nombre = "tempo") {
+	const element = document.createElement("a");
+	element.setAttribute("href", "data:text/plain," + encodeURIComponent(objeto));
+	element.setAttribute("download", nombre + ".json");
+	element.click();
 }
